fffb98267fb20d70fff00b03858f76021e16118b
Prass <vinicius_prass@hotmail.com>
2024-01-24 11:54:31 -0300

    att

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const sequelize = require('./configdb');
+const Usuario = require('./models/Usuarios');
 
 const app = express();
+
+(async () => {
+    try {
+        await sequelize.sync({force: true});
+    } catch (error) {
+        console.error('Erro ao sincronizar: ', error);
+    }
+})();
 
 //Configurar engine do Handlebars
 app.engine('handlebars', exphbs.engine());
--- configdb.js
+++ configdb.js
@@ -1,8 +1,8 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('teste_login', 'root', {
+const sequelize = new Sequelize('teste_login', 'root', '', {
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
 });
 
 module.exports = sequelize;
